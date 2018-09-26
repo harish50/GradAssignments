465153bfa254af02a2957eb1200260c775bad207
S Harish <sharish@akamai.com>
2018-09-26 23:31:55 +0530

    Coding Done

--- DailerApp/src/DailerApp.java
+++ DailerApp/src/DailerApp.java
@@ -1,8 +1,69 @@
-import org.junit.Test;
 
 public class DailerApp {
-    @Test
-    public void firstTest(){
 
+    public Contact contacts[] ;
+    DailerApp(){}
+    DailerApp(Contact []contacts){
+        this.contacts   = contacts;
+    }
+    public String searchWithDialPad(String dialPadString){
+        String output= "NotFound";
+        Boolean last,phnNumber1,phnNumber2;
+        last    = false;
+        phnNumber1= false;
+        phnNumber2=false;
+        for(Contact contact:contacts){
+            if (dialPadString.equals(convertToNumber(contact.firstName))){
+                return contact.firstName+contact.lastName;
+            }
+            else if (!last && dialPadString.equals(convertToNumber(contact.lastName))){
+                    last    = true;
+                    output  = contact.firstName+contact.lastName;
+
+            }
+            else if (!last && !phnNumber1 && dialPadString.equals(contact.phone.split(" ")[0])){
+                    phnNumber1  = true ;
+                    output  = contact.firstName + contact.lastName;
+
+            }
+            else if (!last && !phnNumber1 && !phnNumber2 && dialPadString.equals(contact.phone.split(" ")[1])){
+                    phnNumber2  = true;
+                    output  = contact.firstName + contact.lastName;
+            }
+        }
+        return output;
+    }
+    public  String convertToNumber(String str){
+        String newStr   = "";
+        str             = str.toLowerCase();
+        for(int i=0;i<str.length();i++){
+            char c  = str.charAt(i);
+
+            if(c>=97 && c<=99){
+                newStr=newStr.concat("2");
+            }
+            else if(c>=100 && c<=102){
+                newStr=newStr.concat("3");
+            }
+            else if(c>=103 && c<=105){
+                newStr =  newStr.concat("4");
+            }
+            else if(c>=106 && c<=108){
+                newStr=newStr.concat("5");
+            }
+            else if(c>=109 && c<=111){
+                newStr=newStr.concat("6");
+            }
+            else if(c>=112 && c<=115){
+                newStr=newStr.concat("7");
+            }
+            else if (c>=116 && c<=118){
+                newStr=newStr.concat("8");
+            }
+            else if (c>=119 && c<=122) {
+                newStr=newStr.concat("9");
+            }
+        }
+        return newStr;
     }
 }
--- DailerApp/src/DailerTest.java
+++ DailerApp/src/DailerTest.java
@@ -1,11 +1,39 @@
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class DailerTest {
     @Test
     public void test1(){
-        Assert.assertEquals("242",Contact.convertToNumer("aga"));
+        DailerApp dailerApp = new DailerApp();
+        Assert.assertEquals("242",dailerApp.convertToNumber("aga"));
+    }
+
+    @Test
+    public void test2(){
+        DailerApp dailerApp = new DailerApp();
+        Assert.assertEquals("874742",dailerApp.convertToNumber("Trisha"));
+    }
+
+    @Test
+    public void searchWithNumpadTest(){
+        Contact contact = new Contact("Trisha","","97432 74880","");
+        Contact[] contacts= new Contact[1];
+        contacts[0]=contact;
+        DailerApp dailerApp = new DailerApp(contacts);
+        Assert.assertEquals("Trisha", dailerApp.searchWithDialPad("874742"));
+    }
+    @Test
+    public void searchWithNumpad(){
+        Contact[] contacts= new Contact[4];
+        contacts[0] = new Contact("Bradd","Pitt","96877 83241","");
+        contacts[1] = new Contact("Trisha","","97432 74880","");
+        contacts[2] = new Contact("Keanu","Reeves","96857 68789","");
+        contacts[3] = new Contact("Vijay","Sethupathy","99490 83597","");
+        DailerApp dailerApp = new DailerApp(contacts);
+        Assert.assertEquals("Trisha", dailerApp.searchWithDialPad("874742"));
+        Assert.assertEquals("BraddPitt", dailerApp.searchWithDialPad("7488"));
+        Assert.assertEquals("VijaySethupathy",dailerApp.searchWithDialPad("99490"));
+        Assert.assertEquals("Trisha",dailerApp.searchWithDialPad("74880"));
     }
 
 }
