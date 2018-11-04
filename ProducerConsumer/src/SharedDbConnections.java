1366cab59e169aa24a3235215902c4b45d40d116
S Harish <sharish@akamai.com>
2018-11-04 23:04:42 +0530

    ProducerConsumer with 5 consumers and DBConnections sharedresource Added capacity in SharedResource class

--- ProducerConsumer/src/SharedDbConnections.java
+++ ProducerConsumer/src/SharedDbConnections.java
@@ -3,9 +3,9 @@ import java.util.ArrayList;
 
 public class SharedDbConnections {
     private ArrayList<Connection> connectionsList = new ArrayList<>();
-
+    private int capacity=25;
     public synchronized void produce(Connection connection) throws InterruptedException {
-        while (this.connectionsList.size() == 25) {
+        while (this.connectionsList.size() >capacity) {
             wait();
         }
         System.out.println("Produced" + this.connectionsList.size());
@@ -14,7 +14,7 @@ public class SharedDbConnections {
     }
 
     public synchronized Connection consume() throws InterruptedException {
-        while (this.connectionsList.size() <= 0) {
+        while (this.connectionsList.size() <1) {
             wait();
         }
         Connection connection = this.connectionsList.get(0);
